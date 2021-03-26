82af34a29cacebd8255c19abda175802a16aa601
Matias-Pagano <paganomatias@hotmail.com>
2021-03-25 21:20:13 -0300

    Matias Pagano - subo el register.html

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 8000
+const port = 3000
 const path = require('path');
 
 app.use(express.static('public'));
@@ -8,6 +8,14 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'));
+});
+
+app.get('/Register', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'));
+});
+
+app.get('/Login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'));
 });
 
 app.listen(port, () => {
